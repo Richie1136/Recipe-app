8a64b4a2e537dd8c61408f3583213ce7fb03408e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-25 10:33:31 -0400

    Using styled components for some styling and then mapping over qll of the recipes that i got back from the spoonacular api and outputting the recipe title and image

--- recipeapp/src/components/row/Row.js
+++ recipeapp/src/components/row/Row.js
@@ -1,34 +1,50 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import styled from 'styled-components'
 
 const Row = ({ title }) => {
   const [recipe, setRecipe] = useState([])
   const KEY = process.env.REACT_APP_FOOD_API_KEY
 
-  const searchRandom = async () => {
+  const searchRandom = useCallback(async () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${KEY}&number=5`)
     const data = await api.json()
     console.log(data)
     setRecipe(data.recipes)
-  }
+  }, [KEY])
 
   useEffect(() => {
     searchRandom()
-  }, [])
+  }, [searchRandom])
 
 
   return (
     <div className='row'>
-      <h2>{title}</h2>
       {recipe.map((rec) => {
         return (
-          <div key={rec.id}>
-
-            <p>{rec.title}</p>
-          </div>
+          <Wrapper>
+            <h3>Trending</h3>
+            {recipe.map((rec) => {
+              return (
+                <Card>
+                  <p>{rec.title}</p>
+                  <img src={rec.image} alt='' />
+                </Card>
+              )
+            })}
+          </Wrapper>
         )
       })}
     </div>
   )
 }
+
+const Wrapper = styled.div`
+margin: 4rem 0rem
+`
+
+const Card = styled.div`
+min-height: 25rem
+border-rqdius: 2rem
+`
 
 export default Row
