55142f9e2d308eae0c9549a28d88f08c2d446f78
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-30 09:59:51 -0400

    Setting up the links to the different types of food

--- recipeapp/src/components/categories/Categories.js
+++ recipeapp/src/components/categories/Categories.js
@@ -1,11 +1,30 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa'
-import { GiNoodles, GiChopSticks } from 'react-icons/fa'
+import { GiNoodles, GiChopsticks } from 'react-icons/gi'
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
+import { List } from '../../List'
 
 const Categories = () => {
   return (
     <>
-      <div>Categories</div>
-      <FaHamburger />
+      <List>
+        <NavLink to={'/Italian'}>
+          <FaPizzaSlice />
+          <h4>Italian</h4>
+        </NavLink>
+        <NavLink to={'/American'}>
+          <FaHamburger />
+          <h4>American</h4>
+        </NavLink>
+        <NavLink to={'/Thai'}>
+          <GiNoodles />
+          <h4>Thai</h4>
+        </NavLink>
+        <NavLink to={'/Japanese'}>
+          <GiChopsticks />
+          <h4>Japanese</h4>
+        </NavLink>
+      </List>
     </>
   )
 }
