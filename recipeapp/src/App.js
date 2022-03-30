a9ab7c2385c4f6c3dffdc04ccef286f3467fd057
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-30 09:40:20 -0400

    Created a new component and imported from icons from react

--- recipeapp/src/App.js
+++ recipeapp/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Trending from './components/trending/Trending'
 import Search from './components/search/Search';
 import Veggie from './components/veggie/Veggie';
+import Categories from './components/categories/Categories';
 
 function App() {
   return (
     <div className="App">
       <Search />
+      <Categories />
       <Trending />
       <Veggie />
     </div>
---
+++ recipeapp/src/components/categories/Categories.js
@@ -0,0 +1,13 @@
+import { FaPizzaSlice, FaHamburger } from 'react-icons/fa'
+import { GiNoodles, GiChopSticks } from 'react-icons/fa'
+
+const Categories = () => {
+  return (
+    <>
+      <div>Categories</div>
+      <FaHamburger />
+    </>
+  )
+}
+
+export default Categories
