7bb9dd9fd8ab1228e0ff9968e0086c07b7fd5628
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-31 10:03:55 -0400

    Rendering italian food using the spoonacular api

--- recipeapp/src/components/italian/Italian.js
+++ recipeapp/src/components/italian/Italian.js
@@ -1,11 +1,56 @@
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState, useEffect, useCallback } from 'react'
 
 const Italian = () => {
+
+  const [italian, setItalian] = useState([])
+
+  const KEY = process.env.REACT_APP_FOOD_API_KEY
+
+  const getItalian = useCallback(async () => {
+
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${KEY}&number=5&tags=italian`)
+    const data = await api.json()
+    setItalian(data.recipes)
+  }, [KEY])
+
+  useEffect(() => {
+    getItalian()
+  }, [getItalian])
   return (
-    <div>Italian</div>
+    <Grid>
+      {italian.map((item) => {
+        return (
+          <Card key={item.id}>
+            <h4>{item.title}</h4>
+            <img src={item.image} alt={item.title} />
+          </Card>
+        )
+      })}
+    </Grid>
   )
 }
+
+const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+grid-gap: 3rem;
+`
+
+const Card = styled.div`
+img {
+  border-radius: 32px;
+  width: 100%;
+}
+a {
+  text-decoration: none;
+}
+h4 {
+  text-align: center;
+  padding: 16px;
+}
+`
 
 export default Italian
