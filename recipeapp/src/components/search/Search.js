c49add3db3e5b884753c99a5bf12d4801ea8d72b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-04 11:08:58 -0400

    Grabbing the input value and also preventing my form from refreshing the page

--- recipeapp/src/components/search/Search.js
+++ recipeapp/src/components/search/Search.js
@@ -4,9 +4,20 @@ import { FaSearch } from 'react-icons/fa'
 
 
 const Search = () => {
+
+  const [input, setInput] = useState('')
+
+  const handleInput = (event) => {
+    setInput(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
   return (
-    <form className='search'>
-      <input type='text' placeholder='Search an Item' />
+    <form className='search' onSubmit={handleSubmit}>
+      <input type='text' value={input} onChange={handleInput} placeholder='Search an Item' />
       <div className='search-logo'>
         <i className="fa-solid fa-magnifying-glass">
           <FaSearch />
