48bfa463693acdb56780fb06b870c111c4fe67e1
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-04 10:58:19 -0400

    Changed some of the styling for the search component

--- recipeapp/src/components/categories/Categories.js
+++ recipeapp/src/components/categories/Categories.js
@@ -58,6 +58,13 @@ svg {
 
 &.active {
   background: linear-gradient(to right, #f27121, #e94057)
+
+  svg {
+    color: white;
+  }
+  h4 {
+    color: white;
+  }
 }
 
 `
--- recipeapp/src/components/search/Search.js
+++ recipeapp/src/components/search/Search.js
@@ -1,13 +1,18 @@
 import './Search.css'
+import { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
+
 
 const Search = () => {
   return (
-    <div className='search'>
+    <form className='search'>
       <input type='text' placeholder='Search an Item' />
       <div className='search-logo'>
-        <i className="fa-solid fa-magnifying-glass"></i>
+        <i className="fa-solid fa-magnifying-glass">
+          <FaSearch />
+        </i>
       </div>
-    </div>
+    </form>
   )
 }
 
