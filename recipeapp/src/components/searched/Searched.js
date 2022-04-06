5f3f66e70b8cb0046ad3e1aa37daaaad14f1ee3a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-06 09:08:39 -0400

    Using the useParams hook from react router dom inside of my useEffect hook to re render the app when the search field changes

--- recipeapp/src/components/searched/Searched.js
+++ recipeapp/src/components/searched/Searched.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Searched = () => {
+  const [getSearched, setGetSearched] = useState([])
+
+  let params = useParams()
+
+  const KEY = process.env.REACT_APP_FOOD_API_KEY
+
+  const getSearchedItem = useCallback(async (name) => {
+
+
+    const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${KEY}&query=${name}`)
+    const data = await api.json()
+    setGetSearched(data.results)
+  }, [KEY])
+
+  useEffect(() => {
+    getSearchedItem(params.search)
+  }, [getSearchedItem, params.search])
+
   return (
-    <div>Searched</div>
+    <div>
+      <h2>Searched</h2>
+      <h4>{getSearched}</h4>
+    </div>
   )
 }
 
