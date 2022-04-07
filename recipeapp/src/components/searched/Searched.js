b9495ffb115bdf587f0688c946ace744851603da
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-07 10:06:21 -0400

    Imported the CardWrapper and Grid styled component inside of the search component since the styling is the same as the Food component

--- recipeapp/src/components/searched/Searched.js
+++ recipeapp/src/components/searched/Searched.js
@@ -2,6 +2,8 @@ import { useState, useEffect, useCallback } from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { CardWrapper } from '../../CardWrapper'
+import { Grid } from '../../Grid'
 
 
 const Searched = () => {
@@ -25,37 +27,16 @@ const Searched = () => {
     <Grid>
       {getSearched.map((search) => {
         return (
-          <Card key={search.id}>
+          <CardWrapper key={search.id}>
             <Link to={`/recipe/${search.id}`}>
               <h4>{search.title}</h4>
               <img src={search.image} alt={search.title} />
             </Link>
-          </Card>
+          </CardWrapper>
         )
       })}
     </Grid>
   )
 }
-
-
-const Grid = styled.div`
-display: grid;
-grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-grid-gap: 3rem;
-`
-
-const Card = styled.div`
-img {
-  border-radius: 32px;
-  width: 100%;
-}
-a {
-  text-decoration: none;
-}
-h4 {
-  text-align: center;
-  padding: 16px;
-}
-`
 
 export default Searched
