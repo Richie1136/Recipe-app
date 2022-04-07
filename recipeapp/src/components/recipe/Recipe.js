00b774c8a73573213a940a53c3941f172c2d09e8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-07 09:16:01 -0400

    Conditionally rendering the active class on the two buttons

--- recipeapp/src/components/recipe/Recipe.js
+++ recipeapp/src/components/recipe/Recipe.js
@@ -5,6 +5,8 @@ import styled from 'styled-components'
 const Recipe = () => {
 
   const [recipeDetails, setRecipeDetails] = useState({})
+  const [activeTab, setActiveTab] = useState('instructions')
+
 
   const KEY = process.env.REACT_APP_FOOD_API_KEY
 
@@ -23,12 +25,53 @@ const Recipe = () => {
 
 
   return (
-    <div>
-      <h2 style={{ 'marginBottom': '10px' }}>{recipeDetails.title}</h2>
-      <img src={recipeDetails.image} alt={recipeDetails.title}></img>
+    <DetailWrapper>
+      <div>
+        <h2 style={{ 'marginBottom': '10px' }}>{recipeDetails.title}</h2>
+        <img src={recipeDetails.image} alt={recipeDetails.title}></img>
+      </div>
+      <Info>
+        <Button className={activeTab === 'instructions' ? 'active' : ''} onClick={() => setActiveTab('instructions')}>Instructions</Button>
+        <Button className={activeTab === 'ingredients' ? 'active' : ''} onClick={() => setActiveTab('ingredients')}>Ingredients</Button>
+      </Info>
       <p style={{ 'width': '50%', 'marginTop': '15px' }}>{recipeDetails.instructions}</p>
-    </div>
+    </DetailWrapper>
   )
 }
+
+const DetailWrapper = styled.div`
+margin-top: 160px;
+margin-bottom: 80px;
+display: flex;
+
+.active {
+  background: linear-gradient(35deg, #494949, #313131);
+  color: white;
+}
+
+h2 {
+  margin-bottom: 32px;
+}
+li {
+  font-size: 19.2px;
+  line-height: 40px;
+}
+ul {
+  margin-top: 32px;
+}
+`
+
+const Button = styled.button`
+padding: 16px 32px;
+color: #313131;
+background-color: white;
+border: 2px solid black;
+margin-right: 32px;
+font-weight: 600;
+`
+
+const Info = styled.div`
+margin-left: 160px;
+`
 
 export default Recipe
